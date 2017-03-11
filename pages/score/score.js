e00f3ebde9099b2481201ac6ba2f233992fc85b6
xhy <695729033@qq.com>
2017-03-12 02:33:29 +0800

    把request给加上去了，
    反正每次都会连接失败，啊哈哈，因为懒得写后端也不要我写后端，对了，还有那个加载中啊什么
    的我也懒得写了，课表到这里应该告一段落了，不过还有那个域名合不合法的问题，再去找找资料，找不到就等后端好了再管！

--- pages/score/score.js
+++ pages/score/score.js
@@ -1,4 +1,6 @@
 var app = getApp()
+
+
 Page({
     data: {
             score: [{Subject:'高数',Fraction: '90',color:'fraction1'}, //#4496DC优秀fraction1
@@ -12,20 +14,69 @@ Page({
               {id: 1,name: '2015-2016-2'},
               {id: 2,name: '2015-2016-1'},
               {id: 3,name: '2014-2015-2'}],
-            index: 0
+            index: 0,
+            alert: "该阶段没有成绩！请重新选择年份！"
         },
+    onLoad: function  (options) {
+        var that = this//接下来如果接着用this会有歧义，所以先转换成that
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo){
+          //更新数据
+          that.setData({
+            userInfo:userInfo
+          })
+        })
+        console.log('onLoad score...')
+        wx.request({
+          url: 'https://www.xiedashuaige.cn/weiccsu/GradeServlet.php',
+          data: {index : that.data.index, score : that.data.score},
+          method: 'GET',
+          success: function(res){
+            console.log(res.data.score.result)
+            that.setData({
+              //Industry:res.data.result
+            })
+          },
+          fail: function() {
+            var cData = that.data.alert
+            cData = "连接服务器失败,请稍后再试!"
+            that.setData({
+                alert: cData
+            })
+          }
+        })
+    },
     bindPickerChange: function(e) {
-        console.log("66"+e.detail.value+"22");
+        var that = this
         console.log('picker发送选择改变，携带值为', e.detail.value)
         this.setData({
             index: e.detail.value
         })
+        wx.request({
+          url: 'https://www.xiedashuaige.cn/weiccsu/GradeServlet.php',
+          data: {index : that.data.index, score : that.data.score},
+          method: 'GET',
+          success: function(res){
+            console.log(res.data.score.result)
+            that.setData({
+              //Industry:res.data.result
+            })
+          },
+          fail: function() {
+            var cData = that.data.alert
+            cData = "连接服务器失败,请稍后再试!"
+            that.setData({
+                alert: cData
+            })
+          }
+        })
+        //更改了picker的选择所以还要ajax一下
         if (e.detail.value == 2) {
             var cData=this.data.score;
             cData=[];//先修改json值
             this.setData({
-            score: cData
-        })
+                score: cData
+            })
         }/*else{
             var cData=this.data.score;
             cData = [{Subject:'高数',Fraction: '90',color:'fraction1'}, //#4496DC优秀fraction1
